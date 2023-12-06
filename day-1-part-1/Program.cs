83af8b5e5ff36c15a60e176dc45921af7ec7910c
Rob Habraken <roberthabraken@hotmail.com>
2023-12-06 22:01:50 +0100

    Add day 1

---
+++ day-1-part-1/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] lines = File.ReadAllLines("..\\..\\..\\input.txt");
+
+var answer = 0;
+foreach (var line in lines)
+{
+    char? firstDigit = null, lastDigit = null;
+    foreach (var character in line)
+    {
+        if (Char.IsDigit(character))
+        {
+            if (!firstDigit.HasValue)
+                firstDigit = character;
+            lastDigit = character;
+        }
+    }
+    var calibrationValue = Int32.Parse($"{firstDigit}{lastDigit}");
+    answer += calibrationValue;
+}
+
+Console.WriteLine(answer);
---
+++ day-1-part-2/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] lines = File.ReadAllLines("..\\..\\..\\input.txt");
+
+string[] spelledDigits = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+var answer = 0;
+foreach (var line in lines)
+{
+    var firstDigitIndex = int.MaxValue;
+    var lastDigitIndex = -1;
+
+    char? firstDigit = null, lastDigit = null;
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (Char.IsDigit(line, i))
+        {
+            if (!firstDigit.HasValue)
+            {
+                firstDigitIndex = i;
+                firstDigit = line[i];
+            }
+            lastDigitIndex = i;
+            lastDigit = line[i];
+        }
+    }
+
+    for (var i = 0; i < spelledDigits.Length; i++)
+    {
+        var first = line.IndexOf(spelledDigits[i]);
+        var last = line.LastIndexOf(spelledDigits[i]);
+
+        if (first > -1 && first < firstDigitIndex)
+        {
+            firstDigitIndex = first;
+            firstDigit = (i + 1).ToString()[0];
+        }
+
+        if (last > lastDigitIndex)
+        {
+            lastDigitIndex = last;
+            lastDigit = (i + 1).ToString()[0];
+        }
+    }
+    
+    var calibrationValue = Int32.Parse($"{firstDigit}{lastDigit}");
+    answer += calibrationValue;
+}
+
+Console.WriteLine(answer);
