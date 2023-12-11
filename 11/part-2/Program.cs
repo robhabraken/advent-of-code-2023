ff8cb935cd809de587a171ce5996661b28c58073
Rob Habraken <roberthabraken@hotmail.com>
2023-12-11 09:39:07 +0100

    Add day 11 part 2

---
+++ 11/part-2/Program.cs
@@ -0,0 +1,65 @@
+﻿string[] lines = File.ReadAllLines("..\\..\\..\\..\\input.txt");
+
+long answer = 0;
+var galaxies = new List<Galaxy>();
+var expansionFactor = 999999;
+
+// read image and expand rows while doing so
+var expandY = 0;
+for (var y = 0; y < lines.Length; y++)
+{
+    var rowEmpty = true;
+    for (var x = 0; x < lines[0].Length; x++)
+    {
+        if (lines[y][x].Equals('#'))
+        {
+            galaxies.Add(new Galaxy(x, y + expandY));
+            rowEmpty = false;
+        }
+    }
+    if (rowEmpty)
+        expandY += expansionFactor;
+}
+
+// find columns to expand
+var emptyColumns = new List<int>();
+for (var x = 0; x < lines[0].Length; x++)
+{
+    var columnEmpty = true;
+    foreach (var galaxy in galaxies)
+        if (galaxy.x == x)
+            columnEmpty = false;
+
+    if (columnEmpty)
+        emptyColumns.Add(x);
+}
+
+// expand columns
+for (var i = emptyColumns.Count - 1; i >= 0; i--)
+    foreach (var galaxy in galaxies)
+        if (galaxy.x > emptyColumns[i])
+            galaxy.x += expansionFactor;
+
+// calculate the sum of the shortest paths
+for (var i = 0; i < galaxies.Count; i++)
+    for (var j = i; j < galaxies.Count; j++)
+        answer += galaxies[i].DistanceTo(galaxies[j]);
+
+ Console.WriteLine(answer);
+
+class Galaxy
+{
+    public int x;
+    public int y;
+
+    public Galaxy(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int DistanceTo(Galaxy other)
+    {
+        return Math.Abs(this.x - other.x) + Math.Abs(this.y - other.y);
+    }
+}
