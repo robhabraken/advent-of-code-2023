e9e262f056e2ab26bf723d35e0e03ba1536e13bc
Rob Habraken <roberthabraken@hotmail.com>
2023-12-13 11:05:05 +0100

    Add day 13 part 1

---
+++ 13/part-1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\input.txt");
+
+long answer = 0;
+var pattern = new List<string>();
+for (var i = 0; i < lines.Length; i++)
+{
+    if (!string.IsNullOrEmpty(lines[i].Trim()))
+        pattern.Add(lines[i]);
+
+    if (string.IsNullOrEmpty(lines[i].Trim()) || i == lines.Length - 1)
+        processPattern();
+}
+
+Console.WriteLine(answer);
+
+void processPattern()
+{
+    var patternNote = analyzePattern() * 100;
+    if (patternNote < 0)
+    {
+        rotate();
+        patternNote = analyzePattern();
+    }
+    answer += patternNote;
+
+    pattern = new List<string>();
+}
+
+int analyzePattern()
+{
+    var findAnchors = new List<int>();
+    for (var i = 0; i < pattern.Count; i++)
+        if (i < pattern.Count - 1)
+            if (pattern[i].Equals(pattern[i + 1]))
+                findAnchors.Add(i);
+
+    if (findAnchors.Count > 0)
+        foreach (var anchor in findAnchors)
+            if (validateAnchor(anchor))
+                return anchor + 1;
+
+    return -1;
+}
+
+bool validateAnchor(int index)
+{
+    var distanceFromEdge = Math.Max(pattern.Count - index - 2, index);
+    for (var delta = 1; delta <= distanceFromEdge; delta++)
+        if (index - delta >= 0 && index + delta + 1 < pattern.Count &&
+            !pattern[index - delta].Equals(pattern[index + delta + 1]))
+            return false;
+
+    return true;
+}
+
+void rotate()
+{
+    var rotatedPattern = new List<string>();
+    for (var x = 0; x < pattern[0].Length; x++)
+    {
+        var row = string.Empty;
+        for (var y = pattern.Count - 1; y >= 0; y--)
+            row += pattern[y][x];
+
+        rotatedPattern.Add(row);
+    }
+    pattern = rotatedPattern;
+}
