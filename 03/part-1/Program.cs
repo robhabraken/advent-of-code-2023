c6b40d007bbe152df028a48c6478a9f0aa067b2a
Rob Habraken <roberthabraken@hotmail.com>
2023-12-15 23:51:31 +0100

    Use simple types

--- 03/part-1/Program.cs
+++ 03/part-1/Program.cs
@@ -6,23 +6,23 @@ for (var line = 0; line < lines.Length; line++)
     var startIndex = -1;
     for (var character = 0; character < lines[line].Length; character++)
     {
-        if (Char.IsDigit(lines[line][character]))
+        if (char.IsDigit(lines[line][character]))
         {
             if (startIndex == -1)
                 startIndex = character;
 
-            if (character == lines[line].Length - 1 || !Char.IsDigit(lines[line][character + 1]))
+            if (character == lines[line].Length - 1 || !char.IsDigit(lines[line][character + 1]))
             {
                 var validNumber = false;
                 for (var row = line - 1; row <= line + 1; row++)
                     for (var column = startIndex - 1; column <= character + 1; column++)
                         if (row >= 0 && row < lines.Length &&
                             column >= 0 && column < lines[row].Length)
-                            if (lines[row][column] != '.' && !Char.IsDigit(lines[row][column]))
+                            if (lines[row][column] != '.' && !char.IsDigit(lines[row][column]))
                                 validNumber = true;
 
                 if (validNumber)
-                    answer += Int32.Parse(lines[line].Substring(startIndex, character - startIndex + 1));
+                    answer += int.Parse(lines[line].Substring(startIndex, character - startIndex + 1));
 
                 startIndex = -1;
             }
