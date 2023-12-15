9517c5f5e10bcd078f57ac718629e5e173afc54f
Rob Habraken <roberthabraken@hotmail.com>
2023-12-15 09:00:57 +0100

    Add day 15 part 1

---
+++ 15/part-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\input.txt");
+
+long answer = 0;
+var steps = lines[0].Split(',');
+
+foreach (var  step in steps)
+{
+    var currentValue = 0;
+    var asciiValues = Encoding.ASCII.GetBytes(step);
+    foreach (var value in asciiValues)
+    {
+        currentValue += value;
+        currentValue *= 17;
+        currentValue %= 256;
+    }
+    answer += currentValue;
+}
+
+Console.WriteLine(answer);
