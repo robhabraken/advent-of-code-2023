83d4043ade51cd5f958b7e3e49add264a78ad934
Rob Habraken <roberthabraken@hotmail.com>
2023-12-15 23:47:35 +0100

    Remove curly brackets to make code more compact

--- 03/part-1/Program.cs
+++ 03/part-1/Program.cs
@@ -9,25 +9,17 @@ for (var line = 0; line < lines.Length; line++)
         if (Char.IsDigit(lines[line][character]))
         {
             if (startIndex == -1)
-            {
                 startIndex = character;
-            }
+
             if (character == lines[line].Length - 1 || !Char.IsDigit(lines[line][character + 1]))
             {
                 var validNumber = false;
                 for (var row = line - 1; row <= line + 1; row++)
-                {
                     for (var column = startIndex - 1; column <= character + 1; column++)
-                    {
                         if (row >= 0 && row < lines.Length &&
                             column >= 0 && column < lines[row].Length)
-                        {
-                            if (lines[row][column] != '.' && !Char.IsDigit(lines[row][column])) {
+                            if (lines[row][column] != '.' && !Char.IsDigit(lines[row][column]))
                                 validNumber = true;
-                            }
-                        }
-                    }
-                }
 
                 if (validNumber)
                     answer += Int32.Parse(lines[line].Substring(startIndex, character - startIndex + 1));
