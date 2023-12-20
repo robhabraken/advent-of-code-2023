e0a191a3a32570f4e8465abaef2bea183a9a799f
Rob Habraken <roberthabraken@hotmail.com>
2023-12-20 19:50:38 +0100

    Add day 20 part 2

--- 20/part-2/Program.cs
+++ 20/part-2/Program.cs
@@ -2,8 +2,9 @@
 
 var pulseQueue = new Queue<Pulse>();
 var modules = new Dictionary<string, Module>();
+var finalMachineName = "rx";
 
-var answer = 0;
+long answer = 1;
 
 // read input
 foreach (var line in lines)
@@ -14,46 +15,104 @@ foreach (var line in lines)
 
 // register inputs for all conjuction modules
 foreach (var module in modules.Values)
-    if (module.type == ModuleType.Conjuction)
-        foreach (var otherModule in modules.Values)
-            foreach (var dest in otherModule.destinations)
-                if (dest.Equals(module.name))
-                    module.RegisterInput(otherModule);
+    foreach (var otherModule in modules.Values)
+        foreach (var dest in otherModule.destinations)
+            if (dest.Equals(module.name))
+                module.RegisterInput(otherModule);
 
 // create memory store to keep track of the last pulse for each input
 foreach (var module in modules.Values)
     if (module.type == ModuleType.Conjuction)
         module.CreateMemoryStore();
 
-while(true)
+/// part 2: find the last node (module) that leeds to the final machine
+var lastModule = new Module();
+foreach (var module in modules.Values)
+    foreach (var dest in module.destinations)
+        if (dest.Equals(finalMachineName))
+            lastModule = module;
+
+// find the cycle size for each module
+var buttonPressed = 0;
+while (true)
 {
-    answer++;
-
-    var rxPulseCount = 0;
-    var lastRxPulse = -1;
-
     // press the button
-    modules["broadcaster"].ProcessPulse(pulseQueue, modules, new Pulse("button", "broadcaster", 0));
+    modules["broadcaster"].ProcessPulse(pulseQueue, modules, new Pulse("button", "broadcaster", 0), ++buttonPressed);
 
-    // handle all of the pulse in sequence using a queue
+    // handle all of the pulses in sequence using a queue
     while (pulseQueue.Any())
     {
         var pulse = pulseQueue.Dequeue();
         if (modules.ContainsKey(pulse.to))
-            modules[pulse.to].ProcessPulse(pulseQueue, modules, pulse);
-
-        if (pulse.to.Equals("rx"))
-        {
-            rxPulseCount++;
-            lastRxPulse = pulse.value;
-        }
+            modules[pulse.to].ProcessPulse(pulseQueue, modules, pulse, buttonPressed);
     }
 
-    if (rxPulseCount == 1 && lastRxPulse == 0)
+    // check if all cycles are discovered
+    var allCyclesFound = true;
+    foreach (var module in modules.Values)
+    {
+        if (module.type == ModuleType.FlipFlop || module.type == ModuleType.Conjuction)
+            if (module.cycleSize == 0)
+            {
+                allCyclesFound = false;
+                break;
+            }
+    }
+
+    // stop pressing the button once we have found all of the cycle sizes
+    if (allCyclesFound)
         break;
 }
 
+// find the first common point for all last cycles in the module chain (the least common multiple)
+// as those modules all have to be sending a high pulse at the same time in order for the last module to output a low pulse
+var primeFactorLists = new List<List<int>>();
+var highestInputValue = 0;
+foreach (var inputModule in lastModule.inputs)
+{
+    var cycleSize = modules[inputModule].cycleSize;
+    primeFactorLists.Add(FindPrimeFactors(cycleSize));
+    if (cycleSize > highestInputValue)
+        highestInputValue = cycleSize;
+}
+
+var mergedPrimeFactors = new List<int>();
+for (var prime = highestInputValue; prime > 1; prime--)
+{
+    var occurrences = 0;
+    foreach (var primeList in primeFactorLists)
+    {
+        var count = primeList.Where(x => x.Equals(prime)).Count();
+        if (count > occurrences)
+            occurrences = count;
+    }
+    if (occurrences > 0)
+        mergedPrimeFactors.Add(prime);
+}
+
+foreach (var prime in mergedPrimeFactors)
+    answer *= prime;
+
 Console.WriteLine(answer);
+
+List<int> FindPrimeFactors(int value)
+{
+    var primeFactors = new List<int>();
+    for (var division = 2; value > 1; division++)
+    {
+        if (value % division == 0)
+        {
+            while (value % division == 0)
+            {
+                value /= division;
+                primeFactors.Add(division);
+            }
+        }
+    }
+    primeFactors.Sort();
+    primeFactors.Reverse();
+    return primeFactors;
+}
 
 class Module
 {
@@ -65,6 +124,11 @@ class Module
 
     public bool onOff;
     public Dictionary<string, int> memory;
+
+    public int lastOutput;
+    public int cycleSize;
+
+    public Module() { }
 
     public Module(string input)
     {
@@ -99,7 +163,7 @@ class Module
             memory.Add(input, 0);
     }
 
-    public void ProcessPulse(Queue<Pulse> queue, Dictionary<string, Module> modules, Pulse pulse)
+    public void ProcessPulse(Queue<Pulse> queue, Dictionary<string, Module> modules, Pulse pulse, int buttonPressed)
     {
         if (type == ModuleType.Broadcast)
         {
@@ -116,6 +180,10 @@ class Module
 
             onOff = !onOff;
 
+            if (lastOutput != output && cycleSize == 0)
+                cycleSize = buttonPressed;
+            lastOutput = output;
+            
             SendPulse(queue, modules, output);
         }
         else if (type == ModuleType.Conjuction)
@@ -130,6 +198,10 @@ class Module
             var output = 0;
             if (!allHigh)
                 output = 1;
+
+            if (lastOutput != output && cycleSize == 0)
+                cycleSize = buttonPressed;
+            lastOutput = output;
 
             SendPulse(queue, modules, output);
         }
